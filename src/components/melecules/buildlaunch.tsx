d56f3dae108c29c0731a8650fa76ae7c1ae5cfdc
gettingdev <j258anwari@gmail.com>
2024-07-04 04:40:14 +0800

    fix maul

--- src/components/melecules/buildlaunch.tsx
+++ src/components/melecules/buildlaunch.tsx
@@ -1,6 +1,6 @@
 export const BuildLaunch = () => {
   return (
-    <div className="text-center hidden text-6xl text-white mt-20 font-ChillaxMedium md:flex flex-col gap-10">
+    <div className="text-center hidden text-6xl text-white font-ChillaxMedium md:flex flex-col gap-10 my-[120px] mb-[150px]">
       <h1 className="leading-[44px]">We <span className="border border-white px-5 py-0 rounded-[32px] text-[#F4A4EC]">build</span>, <span className="border border-white px-5 py-0 rounded-[32px] text-[#F4A4EC]">launch</span></h1>
       <h1 className="leading-[44px]" >and <span className="border border-white px-5 py-0 rounded-[32px] text-[#F4A4EC]">manage</span> websites</h1>
       <h1 className="leading-[44px]" >for businesses</h1>
